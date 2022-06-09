6928a35aa3d5b4da0d4c7ccce6f1d697902ff4f5
nuhman-bititude <nuhman@bititude.com>
2022-06-09 11:55:17 +0530

    db models mongoose

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var mongo=require("../database/connection")
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // res.render('index', { title: 'Express' });
-  res.send("I'm Here")
+  mongo.connection()
+  collection=mongo.collection()
 });
 
 module.exports = router;
