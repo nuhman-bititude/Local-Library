6bdc4b647c4222cf2fb1f9882411b5d533fa9300
nuhman-bititude <105155558+nuhman-bititude@users.noreply.github.com>
2022-06-13 13:20:32 +0530

    added index

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,31 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   // res.render('index', { title: 'Express' });
 
-  res.redirect('/catalog')
+  res.send("""
+           /book/create
+           /books
+           /book/:id
+           /book/delete/:id
+           /book/update/:id
+           <br>
+           /author/create
+           /authors
+           /author/:id
+           /author/delete/:id
+           /author/update/:id
+           <br>
+           /genre/create
+           /genres
+           /genre/:id
+           /genre/delete/:id
+           /genre/update/:id
+           <br>
+           /bookinstance/create
+           /bookinstances
+           /bookinstance/:id
+           /bookinstance/delete/:id
+           /bookinstance/update/:id
+           """)
 });
 
 module.exports = router;
