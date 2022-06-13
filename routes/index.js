3cc6d80ec0f2c1f953fc0c2e60faae5207db3d2a
nuhman-bititude <nuhman@bititude.com>
2022-06-13 13:27:33 +0530

    Fixes

--- routes/index.js
+++ routes/index.js
@@ -1,35 +1,12 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get("/", function (req, res, next) {
   // res.render('index', { title: 'Express' });
-
-  res.send("
-           /book/create
-           /books
-           /book/:id
-           /book/delete/:id
-           /book/update/:id
-           <br>
-           /author/create
-           /authors
-           /author/:id
-           /author/delete/:id
-           /author/update/:id
-           <br>
-           /genre/create
-           /genres
-           /genre/:id
-           /genre/delete/:id
-           /genre/update/:id
-           <br>
-           /bookinstance/create
-           /bookinstances
-           /bookinstance/:id
-           /bookinstance/delete/:id
-           /bookinstance/update/:id
-           ")
+  index =
+    "/book/create /books /book/:id /book/delete/:id /book/update/:id <br> /author/create /authors /author/:id /author/delete/:id /author/update/:id <br> /genre/create /genres /genre/:id /genre/delete/:id /genre/update/:id <br> /bookinstance/create /bookinstances /bookinstance/:id /bookinstance/delete/:id /bookinstance/update/:id";
+  res.send(index);
 });
 
 module.exports = router;
