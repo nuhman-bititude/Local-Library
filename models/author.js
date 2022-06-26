e5d3cef2cd7e6eceb191343586c3884ddad77a5e
nuhman-bititude <105155558+nuhman-bititude@users.noreply.github.com>
2022-06-26 11:15:42 +0530

    Update author.js

--- models/author.js
+++ models/author.js
@@ -3,8 +3,8 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var AuthorSchema = new Schema({
-  first_name: { type: String,  minLength: 2 },
-  family_name: { type: String,  minLength: 2 },
+  first_name: { type: String, required: true , minLength: 2 },
+  family_name: { type: String, required: true , minLength: 2 },
   date_of_birth: { type: Date },
   date_of_death: { type: Date },
 });
