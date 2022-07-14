24e2f23553ab4708d2c5169f52814fbd4cdff7ec
nuhman-bititude <nuhman@bititude.com>
2022-07-14 17:04:14 +0530

    auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,15 +4,12 @@ require("dotenv").config();
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization;
-    // const token = req.header("auth-token");
     if (!token) {
       return res.status(401).send("Access Denied");
     }
     const verified = jwt.verify(token, process.env.JWT_PRIVATE_KEY);
-    console.log(verified);
     req.user = verified;
     console.log(req.user);
-    // FIXME: Axios Crashing issue
     next();
   } catch (error) {
     console.log(error);
